d8188721dc838309c1abbe4834ce07d6946b0793
mgm-lt133 <a8122135@gsuite.si.aoyama.ac.jp>
2025-11-08 17:10:11 +0900

    CSS修正

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+// eslint.config.js
+const { FlatCompat } = require("@eslint/eslintrc");
+const path = require("path");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+module.exports = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
