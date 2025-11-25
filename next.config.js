fdd7e33496204bbcb1f7e9b5d98b4ceb20150166
mgm-lt133 <a8122135@gsuite.si.aoyama.ac.jp>
2025-11-25 10:37:09 +0900

    DB URL修正

--- next.config.js
+++ next.config.js
@@ -4,21 +4,21 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com', // Googleアイコン用
+        hostname: 'lh3.googleusercontent.com', // Googleアカウントのアイコン用
       },
       {
         protocol: 'https',
-        hostname: 'i.scdn.co', // 旧Spotify用（念のため）
+        hostname: 'i.scdn.co', // 旧Spotifyのアイコン用（念のため残しています）
       },
       {
-        // あなたのSupabaseのドメイン (例: xxxxx.supabase.co)
-        // Vercelのログでエラーが出る場合は、SupabaseダッシュボードのURLを確認して合わせてください
         protocol: 'https',
-        hostname: 'yboauknclliydigxwtju.supabase.co', 
+        // ▼▼▼ あなたのプロジェクトIDを設定しました ▼▼▼
+        hostname: 'nymwqvdowrvmgzlomsom.supabase.co', 
       },
     ],
   },
   eslint: {
+    // ビルド時にESLintエラーがあってもビルドを続行する
     ignoreDuringBuilds: true,
   },
 };
